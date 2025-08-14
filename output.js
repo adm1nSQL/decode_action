90d242e334d4d92faf161b058f4b00644b7c4c99
action <action@github.com>
2025-08-14 19:30:12 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,37 +1,72 @@
-//Thu Aug 14 2025 19:26:08 GMT+0000 (Coordinated Universal Time)
+//Thu Aug 14 2025 19:30:12 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var _0x56cd45 = function () {
-  var _0x110638 = true;
-  return function (_0x19cb3c, _0x156973) {
-    {
-      var _0x50f6ae = _0x110638 ? function () {
-        if (_0x156973) {
-          var _0x43e735 = _0x156973.apply(_0x19cb3c, arguments);
-          _0x156973 = null;
-          return _0x43e735;
-        }
-      } : function () {};
-      _0x110638 = false;
-      return _0x50f6ae;
-    }
-  };
-}();
-var _0x4a015b = _0x56cd45(this, function () {
-  var _0x30b8ad = typeof window !== "undefined" ? window : typeof process === "object" && typeof require === "function" && typeof global === "object" ? global : this;
-  var _0x2403bd = function () {
-    {
-      var _0x30d915 = new _0x30b8ad.RegExp("^([^ ]+( +[^ ]+)+)+[^ ]}");
-      return !_0x30d915.test(_0x4a015b);
-    }
-  };
-  return _0x2403bd();
-});
-_0x4a015b();
-var _0x53b3ca = ["gdC+N8tg08B0aFnzU3dDbUX/OhP4+YUpoie60dunnev8PX5/wJEVbu7x7J5h8hQONrAaixPomMpnW7l6+o4KW3Su98mfWeaDS1Pnr2DNY+J6T6pE/KP2RUJtyMVaYw5KJUXNwG1E9XiJL/ayJUa53Q==", "replace", "body", "by伟人_:糖心VlOG"];
-body = $request[_0x53b3ca[2]][_0x53b3ca[1]](/.+/, _0x53b3ca[0]);
-$notify(_0x53b3ca[3]);
-var _0x352879 = {
+var encode_version = "jsjiami.com.v5";
+body = $request.body.replace(/.+/, "gdC+N8tg08B0aFnzU3dDbWyfsUn3lZ219LTtQ96wJW+WUK2m7t1USHXI6UyaTUxO7SBh/U1BnK9Tme+ClwlJAHSu98mfWeaDS1Pnr2DNY+JPfAwfkSxhQuWrcSEmNe05Qigq1e2xof1Iz/aJmPlKBg==");
+$notify("伟人提醒糖心Vlog会员注入成功!");
+$done({
   body: body
-};
-$done(_0x352879);
+});
+(function (_0x3291f8, _0x36cc9d, _0x5c530e) {
+  var _0x3a04f5 = function () {
+    {
+      var _0x39f4c1 = true;
+      return function (_0x42bd0d, _0x101ea9) {
+        {
+          var _0x65c326 = _0x39f4c1 ? function () {
+            {
+              if (_0x101ea9) {
+                var _0x268869 = _0x101ea9.apply(_0x42bd0d, arguments);
+                _0x101ea9 = null;
+                return _0x268869;
+              }
+            }
+          } : function () {};
+          _0x39f4c1 = false;
+          return _0x65c326;
+        }
+      };
+    }
+  }();
+  var _0x5d45e1 = _0x3a04f5(this, function () {
+    var _0x427fb6 = function () {};
+    var _0x530553 = typeof window !== "undefined" ? window : typeof process === "object" && typeof require === "function" && typeof global === "object" ? global : this;
+    if (!_0x530553.console) {
+      _0x530553.console = function (_0x59c436) {
+        {
+          var _0x5c530e = {};
+          _0x5c530e.log = _0x59c436;
+          _0x5c530e.warn = _0x59c436;
+          _0x5c530e.debug = _0x59c436;
+          _0x5c530e.info = _0x59c436;
+          _0x5c530e.error = _0x59c436;
+          _0x5c530e.exception = _0x59c436;
+          _0x5c530e.trace = _0x59c436;
+          return _0x5c530e;
+        }
+      }(_0x427fb6);
+    } else {
+      _0x530553.console.log = _0x427fb6;
+      _0x530553.console.warn = _0x427fb6;
+      _0x530553.console.debug = _0x427fb6;
+      _0x530553.console.info = _0x427fb6;
+      _0x530553.console.error = _0x427fb6;
+      _0x530553.console.exception = _0x427fb6;
+      _0x530553.console.trace = _0x427fb6;
+    }
+  });
+  _0x5d45e1();
+  _0x5c530e = "al";
+  try {
+    _0x5c530e += "ert";
+    _0x36cc9d = encode_version;
+    if (!(typeof _0x36cc9d !== "undefined" && _0x36cc9d === "jsjiami.com.v5")) {
+      _0x3291f8[_0x5c530e]("删除版本号，js会定期弹窗，还请支持我们的工作");
+    }
+  } catch (_0x11f342) {
+    {
+      _0x3291f8[_0x5c530e]("删除版本号，js会定期弹窗");
+    }
+  }
+})(window);
+encode_version = "jsjiami.com.v5";
